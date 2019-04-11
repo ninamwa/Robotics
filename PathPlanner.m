bfd5afbc06112db9ff6cc3d982b0542b50cdb2c1
charlotteheggem <charlotteheggem@hotmail.com>
2019-04-11 10:22:54 +0100

    path planner and text file

--- PathPlanner.m
+++ PathPlanner.m
@@ -1,7 +1,9 @@
+
 function Waypoints = PathPlanner()
+
 %% Parameters
 robotRadius = 0.2; % was 0.2;
-resolution = 0.0500000007451; % m/cell
+%resolution = 28.5*1000/430; % mm/cell
 NumNodes=1300; % gir smudere resultat kan tunes
 NumNodes= 1000; %1300; % gir smudere resultat kan tunes
 ConnectionDistance = 60; % høyere gir færre punkter % kan tunes
@@ -58,5 +60,8 @@ path = resultPath;
 %    viz(pose,path);
 %end
 %robo = InitController(map2,path,0.3,2,0.5)
-Waypoints = ([xx,yy]-[path(1,1),path(1,2)])*resolution ; % IS THIS CORRECT??
+Waypoints = ([xx,yy]-[path(1,1),path(1,2)])*(28.5/430)*1000; % IS THIS CORRECT??
+
+dlmwrite('Path.txt', Waypoints,'newline','pc');
+% Path = dlmread('Path.txt'); Read Path to matrix
 end
