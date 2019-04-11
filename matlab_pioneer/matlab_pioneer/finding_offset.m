08fb7d9a07c819b2ef7c8bf931670f3a26b5c984
Ophelia Amalie Prillard <Ophelia@Brukers-MacBook-Air-1252.local>
2019-04-11 09:51:25 +0200

    Offset commit

--- matlab_pioneer/matlab_pioneer/finding_offset.m
+++ matlab_pioneer/matlab_pioneer/finding_offset.m
@@ -4,18 +4,14 @@ pioneer_init(sp)
 odometryRes = [];
 odometryRes = [pioneer_read_odometry,odometryRes];
 
-global pioneer_timer
+%global pioneer_timer
 
 for i = 1:50
-    pioneer_timer.StartFcn = @(~,thisEvent)disp([thisEvent.Type ' executed '...
-     datestr(thisEvent.Data.time,'dd-mmm-yyyy HH:MM:SS.FFF')]);
     pioneer_set_controls(sp,50,0);
     odometryRes = [pioneer_read_odometry;odometryRes];
     fprintf('This message is sent at time %s\n', datestr(now,'HH:MM:SS.FFF'));
     pause(4);
     pioneer_set_controls(sp,0,0);
-    pioneer_timer.StopFcn = @(~,thisEvent)disp([thisEvent.Type ' executed '...
-    datestr(thisEvent.Data.time,'dd-mmm-yyyy HH:MM:SS.FFF')]);
 end
 disp(odometryRes);
 pioner_close(sp);
