3dbabd5853cc32ba8fa44652a523ca959af58ed0
charlotteheggem <charlotteheggem@hotmail.com>
2019-05-16 16:36:39 +0100

    tune

--- detect_door_action.m
+++ detect_door_action.m
@@ -7,6 +7,7 @@ global rangescan
 global door_index
 global sp
 pause_turning = 2;
+
 if LeftRightFront == 2
     pioneer_set_controls(sp,0,0);
     pause(1);
@@ -16,7 +17,7 @@ if LeftRightFront == 2
     playSound(status);
     pause(3);
     door_detected_front = false;
-    door_index = door_index+1;
+    door_index = door_index+1;    
 elseif LeftRightFront ==0 || LeftRightFront ==1
     pioneer_set_controls(sp,50,0);
     pause(8);
@@ -27,6 +28,9 @@ elseif LeftRightFront ==0 || LeftRightFront ==1
         first_turn = -45;
         second_turn = 45;
         door_detected_right = false;
+        if door_index == 3
+            second_turn =90; % 180 degrees, dette må nok gjøres for dør i hjørne 3 og :)
+        end
     elseif LeftRightFront ==0 %left
         first_turn = 45;
         second_turn = -45;
