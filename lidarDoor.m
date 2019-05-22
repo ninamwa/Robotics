82a5a8e745dec6863036463eb5eca2a9089eba93
charlotteheggem <charlotteheggem@hotmail.com>
2019-05-22 11:26:44 +0100

    try to fix lidardoor

--- lidarDoor.m
+++ lidarDoor.m
@@ -2,7 +2,7 @@ function ans = lidarDoor(nearby_doors,ranges)  %input: ranges
 % Odometry in mm
 % Start_coordinates in mm, worldcoordinates
 
-% doors: x,y, 0:RIGHT/1:LEFT/2:FRONT, 0:NOTFOUND/1:FOUND/
+% doors: x,y, 0:RIGHT/1:LEFT/2:FRONT, 0:NOTFOUND/1:FOUND/, indexl
 
 %% TODO: (problem) for halvåpen dør kommer max(norm) til å si at max er der 
 %døren går uendelig innover, og ikke dørkarm, som kommer til å gi feil
@@ -26,7 +26,7 @@ points=[x,y];
 leftpoints = [];
 rightpoints = [];
 for i = 1:length(x)
-    if points(i,1)<0 && points(i,2)<2000 % ønsker ikke å kutte liste mer, for out of range problem i løkke
+    if points(i,1)<0 && points(i,2)<2000  % ønsker ikke å kutte liste mer, for out of range problem i løkke
         leftpoints= [points(i,:);leftpoints];
     end
     if points(i,1)>0 && points(i,2)<2000
@@ -34,7 +34,6 @@ for i = 1:length(x)
     end
 end
 %% Door_edit.txt is most likely not correct. measure the first door in world to check.
-ans = [false,false,false];
 
 %% Set thresholds and parameters
 door_threshold = 70; %60? think 70 is too big. How big norm represents a door?
@@ -50,8 +49,6 @@ F_index = 0; % only used for plotting
 nearby_door_left=[];
 nearby_door_right=[];
 nearby_door_front=[];
-
-
 
 %% SET NEARBY DOORS
 if ~isempty(nearby_doors)
@@ -75,20 +72,19 @@ if ~isempty(nearby_door_right)% Right door
         if rightpoints(n,2) < search_range
             % Find all norms, benches and elevator could be detected
             norm_right(n) = norm(rightpoints(n,:)-rightpoints(n+2,:));
-       
             % If the norm is over a threshold, this would be a door
             if norm_right(n) > door_threshold     % Find first maxnorm on rightwall
                 Y = norm_right(n);
                 % Save start position of door
                 right_door = [rightpoints(n,1),rightpoints(n,2)];
-                distance_to_door = rightpoints(n,1);
                 break
             end
         end
     end
     % If the distance to the door is less than door_distance, we
     % want a DOOR event to occur, and list the door as detected
-    if norm(right_door(2))>0 && norm(right_door(2))<door_distance
+    if  norm(right_door(2)) < door_distance % && norm(right_door(2))>0
+        distance_to_door = rightpoints(n,1);
         R_index = n; % used for plotting
         %doors(nearby_door_right(5),4)=1; 
         detect_door_right = true; % SET GLOBAL RIGHT DOOR TO TRUE
@@ -106,12 +102,12 @@ if ~isempty(nearby_door_left)%  If left door
                 Y= norm_left(n);
                 % Save start position of door
                 left_door = [leftpoints(n,1),leftpoints(n,2)];
-                distance_to_door = 1670 - leftpoints(n,1);
                 break
               end
         end
     end
-    if norm(left_door(2))>0 && norm(left_door(2))<door_distance
+    if norm(left_door(2))<door_distance % && norm(left_door(2))>0
+        distance_to_door = 1670 - leftpoints(n,1);
         L_index = n;
         % doors(nearby_door_left(5),4)=1; SET DOOR TO FOUND
         detect_door_left = true; % SET GLOBAL LEFT DOOR TO TRUE
@@ -151,22 +147,15 @@ end
 ans = [detect_door_left, detect_door_right, detect_door_front, distance_to_door];
 
 % Plot rangescan and doors found
-%{
+
 figure(2)
 plot(leftpoints(:,1),leftpoints(:,2))
 hold on
 plot(rightpoints(:,1),rightpoints(:,2))
 hold on
-if L_index ~=0
-    plot(leftpoints(L_index,1),leftpoints(L_index,2),'o')
-    hold on
-end
 if R_index ~=0
     plot(rightpoints(R_index,1),rightpoints(R_index,2),'*')
 end
-if F_index ~=0
-    plot(front_points(F_index,1),front_points(F_index,2),'o')
-end
 hold off
-%}
+
 end
