1ef6ca68c38316dc7608a7ca23f0da2c790f1f43
ninamwa <ninamwa@stud.ntnu.no>
2019-05-23 16:43:58 +0100

    changeReference

---
+++ changeReference.m
@@ -0,0 +1,11 @@
+function bol = changeReference(nr, ref, x_real, y_real)
+    if nr <= 12 && (x_real+150 > ref(1))            
+        bol = true;
+    elseif nr>12 && nr <= 57 && y_real+150 > ref(2)
+        bol = true; 
+    elseif nr >57 && nr <= 87 && x_real-150  < ref(1)
+        bol = true;
+    elseif nr > 87 && nr <= 101 && y_real-150 < ref(2)
+        bol = true;
+    end
+end
