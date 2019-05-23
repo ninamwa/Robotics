71e2f3da8a47ff6a6a54f5fff97b750ed03c2487
charlotteheggem <charlotteheggem@hotmail.com>
2019-05-23 10:57:36 +0100

    correctOdom

---
+++ correctOdometry.m
@@ -0,0 +1,22 @@
+function corrected = correctOdometry(nr,distance_to_wall)
+global odometry
+correction=0;
+if distance_to_wall ~= 0 
+  correction = distance_to_wall - 835; %mm, half hall
+end
+fprintf("odom1: %d, odom2: %d \n", odometry(1), odometry(2))
+if nr <= 12
+    x = odometry(1);
+    y = odometry(2)+correction;
+elseif nr <= 57 
+    x = odometry(1)+ correction;
+    y = odometry(2);
+elseif nr <= 87 
+    x = odometry(1) ;
+    y = odometry(2)- correction;
+elseif nr <= 101 
+    x = odometry(1) - correction;
+    y = odometry(2);
+end
+corrected = [x,y];
+end
