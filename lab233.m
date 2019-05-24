78fd69f1af7390ab9f6c7689a9a339f2aaf41f3e
charlotteheggem <charlotteheggem@hotmail.com>
2019-05-24 22:00:32 +0100

    lab233 without lidar

--- lab233.m
+++ lab233.m
@@ -1,7 +1,19 @@
-function lab233()
+function lab233withlidar()
 delete(timerfindall);
 sp = serial_port_start();
 pioneer_init(sp);
+global odometry;
+global door_index;
+global x_real
+global y_real
+global distance_to_wall
+global number
+door_index = 1;
+number = 1;
+distance_to_wall = 0;
+x_real=odometry(1);
+y_real=odometry(2);
+
 
 odometrytmr = timer('ExecutionMode', 'FixedRate', ...
     'Period', 0.1, ...
@@ -9,33 +21,18 @@ odometrytmr = timer('ExecutionMode', 'FixedRate', ...
     'TimerFcn', {@odometrytimerCallback});
 
 start(odometrytmr);
-global odometry;
+
 
 door_detected_right = false;
 door_detected_left = false;
 door_detected_front = false;
 theta_correction=0;
-global door_index;
-door_index = 1;
 
 doors = dlmread('Doors_edit.txt'); % [x,y,bol,detected] bol=1 right, bol=0 left, bol=2 front
-
-figure(1);
-clf
-axis([-100,20000,-100,20000])
-hold on
-
 reference_path = dlmread('test11.txt');
 
 driveLab(sp,1);
-global x_real
-global y_real
-global distance_to_wall
-global number
-number = 1;
-distance_to_wall = 0;
-x_real=odometry(1);
-y_real=odometry(2);
+
 reference_path(:,1)=reference_path(:,1)*1000+odometry(1);
 reference_path(:,2)=reference_path(:,2)*1000+odometry(2);
 dlmwrite('test11corrected.txt', reference_path,'newline','pc');
@@ -52,15 +49,7 @@ end
 
 x = reference_path(:,1);
 y = reference_path(:,2);
-radius=150;
-for k1 = 1:length(x)
-    plot(x(k1),y(k1),'.');
-    c = [x(k1) y(k1)];
-    pos = [c-radius 2*radius 2*radius];
-    rectangle('Position',pos,'Curvature',[1 1])
-    axis equal
-    text(x(k1) + 0.1,y(k1) + 0.1 ,num2str(k1),'Color','k')
-end
+
 
 for h =1:length(reference_path(:,1))
     number = h;
@@ -75,42 +64,57 @@ for h =1:length(reference_path(:,1))
     while norm([x_real,y_real]-ref)>150
 
         nearby_doors = doors_in_range(doors,[x_real,y_real]);
-        if ~isempty(nearby_doors)
-            disp('in nearby doors')
-            door_detected_right = true;
-            distance_to_door = 0;
+         if ~isempty(nearby_doors)
+             disp('in nearby doors');
+            %rangescan = LidarScan(lidar);
+            %LD_result = lidarDoor(nearby_doors,rangescan);
+            RLF = doors(door_index,3);
+            if RLF == 1
+                door_detected_right = true;
+                nearby_doors = [];
+            elseif RLF == 0
+                door_detected_left = true;
+                nearby_doors = [];
+            else
+                door_index = door_index +1;
+                nearby_doors = [];
+            end
+            %disp(door_detected_right);
+            %distance_to_door = LD_result(5);
+            
+            if door_detected_left == true && door_index ==6
+                door_index = door_index +1;
+                door_detected_left = false;
+            end
+            
         end
 
         if ~door_detected_right && ~door_detected_left && ~door_detected_front
             res = control_system(ref,distance_to_wall,theta_correction,h,theta_ref);
             x_real=res(3);
             y_real=res(4);
-            pioneer_set_controls(sp,res(1)+70,res(2));
-        else         
-            theta_correction = 0;
+            pioneer_set_controls(sp,res(1)+100,res(2));
+        else
             pioneer_set_controls(sp,0,0);
-            pioneer_set_controls(sp,50,0);
-            pause(10);
+            pioneer_set_controls(sp,100,0);
+            pause(5);
             pioneer_set_controls(sp,0,0);
-            
+     
             prompt = 'What is the distance to the wall? ';
             distance_to_wall = input(prompt);
             fprintf('correction %d :',distance_to_wall - 835);
             fprintf('door_index %d : ', door_index);
             if door_index == 3
-                pioneer_set_controls(sp,0,0);
                 pioneer_set_controls(sp,0,45);
                 pause(2);
                 pioneer_set_controls(sp,0,0);
             end
+            
             door_detected_right = false;
+            door_detected_left = false;
+            
             door_index = door_index +1;
             
-            %corrected_odom = correctOdometry(h,distance_to_wall);
-            %x_real = corrected_odom(1);
-            %y_real = corrected_odom(2);
-           
-            % Check if we need to go to the next reference point. list numbers may be tuned
             if changeReference(h,ref,x_real,y_real)
                 break
             end
@@ -153,7 +157,7 @@ function nearby_doors = doors_in_range(doors,odom)
 global door_index;
 start_coordinates  =[3600,2600]; % from lab mm
 %start_coordinates = [6000,7125];% mm from hall
-odom_range_threshold = 600; % How far is odomotry from a existing door?
+odom_range_threshold = 1000; % How far is odomotry from a existing door?
 nearby_doors=[]; % Initialize list to prevent error
 odom_range = norm([doors(door_index,1)-start_coordinates(1),doors(door_index,2)-start_coordinates(2)]-[odom(1),odom(2)]);
 %odom
@@ -162,3 +166,4 @@ if odom_range < odom_range_threshold && doors(door_index,4)==0
     nearby_doors=[nearby_doors;doors(door_index,:),door_index];
 end
 end
+            
