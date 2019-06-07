60b1bfc6e5b06aa39152241241e0093396255f85
ninamwa <ninamwa@stud.ntnu.no>
2019-06-07 12:35:54 +0100

    text changes

--- control_system.m
+++ control_system.m
@@ -56,11 +56,12 @@ v = round(v);
 
 w = v_max*((1+K2*phi/alpha)*(tanh(K1*e)/e)*sin(alpha)+K3*tanh(alpha));
 w = round((w - w_offset)*(180/pi))  ;
-if w > 50
-    w = 20;
-elseif w < -50
-    w = -20;
-end
+
+%if w > 50
+%    w = 20;
+%elseif w < -50
+%    w = -20;
+%end
 
 if isnan(w)
     w=0;
@@ -69,6 +70,6 @@ elseif isnan(v)
 end
 
 
-res = [v,w,x,y];
+res = [v,w];
 
 end
